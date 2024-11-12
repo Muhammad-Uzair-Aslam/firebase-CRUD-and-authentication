107572113132a34374927082384d01dc5cd298be
Uzair Aslam <uzairaslam990@gmail.com>
2024-11-11 23:25:58 -0800

    firebase connection

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCmKvolMm2qF7hSsuazxStfQzjfVE7Iu-U",
+  authDomain: "fir-new-app-cd9ee.firebaseapp.com",
+  projectId: "fir-new-app-cd9ee",
+  storageBucket: "fir-new-app-cd9ee.firebasestorage.app",
+  messagingSenderId: "968576162574",
+  appId: "1:968576162574:web:84f80e95c4a3f642cc155f",
+  measurementId: "G-EC9XBCCRFM"
+};
+
+const app = initializeApp(firebaseConfig);
+const dataB=getFirestore(app);
+export default dataB;
