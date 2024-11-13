f5be2d306a2cdc6327811d832ba22c65cba88de8
Uzair Aslam <uzairaslam990@gmail.com>
2024-11-12 23:28:05 -0800

    deleteButton using firebase

--- app/(components)/deleteButton/deleteButton.js
+++ app/(components)/deleteButton/deleteButton.js
@@ -1,1 +1,23 @@
+"use client"
+import dataB from '@/app/firebaseConfig'
+import { deleteDoc, doc } from 'firebase/firestore'
+import React, { useEffect } from 'react'
 
+
+export default function DeleteButton(props) {
+    const deleteData=async(id)=>{
+        try {
+          const deleted=await deleteDoc(doc(dataB,"newmessage",id))
+          alert("deleted Successfully")
+          window.location.reload()
+          return deleted
+        } catch (error) {
+          console.error("error")
+        }
+      }
+  return (
+    <div>
+        <button className="text-red-500 hover:underline" onClick={()=>deleteData(props.id)}>Delete</button>
+    </div>
+  )
+}
