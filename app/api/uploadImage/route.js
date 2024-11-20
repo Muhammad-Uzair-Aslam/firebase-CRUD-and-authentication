731a0fbf95c60a88a8e1b8d9620979772783ec43
Uzair Aslam <uzairaslam990@gmail.com>
2024-11-19 23:38:05 -0800

    api error resolved

--- app/api/uploadImage/route.js
+++ app/api/uploadImage/route.js
@@ -1,22 +1,16 @@
 import { UploadImage } from "@/lib/uploadImage";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 export const POST = async (req) => {
   try {
     const formdata = await req.formData(); // Parse the incoming form data
     const image = formdata.get("image"); 
     const data= await UploadImage(image,"nextjs-formdata");
-    // const docRef=await addDoc(collection(dataB,"formImages"),{url:data,createdAt:new Date(),})
-    
-    // Extract the "image" field
     if (!image) {
       return NextResponse.json(
         { message: "No image file provided" },
         { status: 400 } // Bad Request
       );
     }
-
-    // You can process or save the image here if needed
-
     return NextResponse.json(
       { message: "Image received successfully", image: image.name ,data:data }, 
       { status: 200 }
